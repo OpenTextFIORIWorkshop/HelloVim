e759254aa440a93f93f721723612a6738600ffcf
OpenTextFIORIWorkshop <oliver.schraag@opentext.com>
2014-11-27 11:20:24 +0000

    Master changes

--- util/formatter.js
+++ util/formatter.js
@@ -5,5 +5,16 @@ jQuery.sap.require("sap.ca.ui.model.format.NumberFormat");
 HelloVim.util.formatter = {
   formatDate:function(iDate){
         return iDate || new Date().toDateString();
-  }  
+  },
+  
+  formatIcon:function(iAmount){
+      var icon;
+      icon = "sap-icon://survey";
+      var option = {
+          groupingSeparator: ".", 
+          decimalSeparator:","};
+      var formater = sap.ui.core.format.NumberFormat.getFloatInstance(option);
+      if (formater.parse(iAmount) > 20000) { icon = "sap-icon://warning2"; }
+      return icon;
+  }
 };
