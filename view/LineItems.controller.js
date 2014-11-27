7c5399674bf77cdbcf6c7a4791221bf112c18f6e
OpenTextFIORIWorkshop <oliver.schraag@opentext.com>
2014-11-27 12:23:09 +0000

    line items view

--- view/LineItems.controller.js
+++ view/LineItems.controller.js
@@ -1,3 +1,22 @@
+jQuery.sap.require("HelloVim.view.LineItemFormatter");
+
 sap.ui.core.mvc.Controller.extend("HelloVim.view.LineItems", {
     
+    onInit: function() {
+        var view = this.getView();
+        
+        	sap.ui.core.UIComponent.getRouterFor(this).attachRouteMatched(function(oEvent) {
+			// when detail navigation occurs, update the binding context
+			if (oEvent.getParameter("name") === "Detail") {
+				var context = new sap.ui.model.Context(view.getModel(), "/" + oEvent.getParameter("arguments").contextPath);
+				view.setBindingContext(context);
+				console.log("Selected item:", context);
+				// Make sure the master is here
+			}
+		}, this);
+        
+    }
+    
+    
 });
+
