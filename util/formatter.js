8c1390ca63d8507f3806ba761f0b56fe9bba9a0c
OpenTextFIORIWorkshop <oliver.schraag@opentext.com>
2014-11-27 10:33:02 +0000

    Date changes

--- util/formatter.js
+++ util/formatter.js
@@ -4,7 +4,6 @@ jQuery.sap.require("sap.ca.ui.model.format.NumberFormat");
 
 HelloVim.util.formatter = {
   formatDate:function(iDate){
-      //var sIntermediate = iDate || new Date();
-        return iDate || "27.11.2014";
+        return iDate || new Date().toDateString();
   }  
 };
