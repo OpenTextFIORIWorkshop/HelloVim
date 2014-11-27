167c7bdbda4adf58cf9257007c701a7b085d0488
OpenTextFIORIWorkshop <oliver.schraag@opentext.com>
2014-11-27 11:14:12 +0000

    Sizing

--- view/Document.view.js
+++ view/Document.view.js
@@ -5,28 +5,12 @@ sap.ui.jsview("HelloVim.view.Document", {
   },
 
   createContent: function() {
- 
-
-//   	this.page = new sap.m.Page({	    
-//       id : "mPage",
-//       showHeader : true,
-//       title: "Becker Berlin",
-//       enableScrolling : false, // !!!! Important !!!!
-//       showFooter: false,
-//       height:500,
-//       content: [ 
          return new sap.ui.core.HTML( {
-          content: 
-    		"<div id=\"divPdf\" style=\"height:100%;width:auto;\">" +
-    	//	"<iframe  id='pdfObject' style=\"height:100%;width:100%\" > </iframe></div>"
-	    		"<object type=\"application/pdf\" id='pdfObject' src='img/po001.pdf' style=\"height:100%;width:100%\" > " +
-	   			    "<param name=\"view\" value=\"FitH\" /> " +
-	    		"</object></div>" 
+          content: "<object type=\"application/pdf\" id='pdfObject' src='img/po001.pdf' style=\"height:102%;width:100%\" > " +
+	   			        "<param name='view' value='Fit' /> " +
+	   			        "<param name='zoom' value='scale' />" +
+	    		    "</object>" 
          });
-	   // 		]
-    // });
-
-    // return this.page;
   }
 
 
